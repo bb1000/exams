db0a3f582cb62bb5497d6835fe9c966caa58afa8
Olav Vahtras <vahtras@kth.se>
2024-03-11 07:38:44 +0100

    backup

---
+++ solutions/__init__.py
@@ -0,0 +1,9 @@
+from .omtenta_20220610 import *
+from .omtenta_20220819 import ( # noqa
+    minmax,
+    minmax_keys,
+    minmax_values,
+    keys_of_minmax_values,
+    minmax_items,
+    minmax2,
+)
